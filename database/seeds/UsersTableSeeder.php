42ad2fe4fb96e0a95558f197fbe1992a5d3c063b
Mohammad Alipour <md.alipour91@gmail.com>
2020-05-08 23:26:02 +0430

    fix seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,23 +1,23 @@
 <?php
-
-use Illuminate\Database\Seeder;
-use App\User;
-use Illuminate\Support\Facades\Hash;
-
-class UsersTableSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        User::query()->truncate();
-        User::create([
-            'email' => 'md.alipour91@gmail.com',
-            'password' => Hash::make('123456'),
-            'name' => 'Administrator'
-        ]);
-    }
-}
+	
+	use App\User;
+	use Illuminate\Database\Seeder;
+	use Illuminate\Support\Facades\Hash;
+	
+	class UsersTableSeeder extends Seeder
+	{
+		/**
+		 * Run the database seeds.
+		 *
+		 * @return void
+		 */
+		public function run()
+		{
+			User::create([
+				'email'         => 'md.alipour91@gmail.com',
+				'password'      => Hash::make('123456'),
+				'mobile_number' => '09120709457',
+				'name'          => 'Administrator'
+			]);
+		}
+	}
