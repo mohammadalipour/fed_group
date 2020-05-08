a59fa0e4196cbfb10b2f5932dbf1c350c55c9e91
Mohammad Alipour <md.alipour91@gmail.com>
2020-05-08 23:35:37 +0430

    fix home page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,18 +4,7 @@
     <div class="body"></div>
     <div class="grad"></div>
     <div class="header">
-        <div>Snapp<span>market</span></div>
-        <p class="description">Sample and Beautiful</p>
-    </div>
-    <br>
-    <div class="login">
-        <div class="login-message"></div>
-        <form method="POST" action="{{ url('/login')}}" id="login-form">
-            <input type="hidden" name="_token" value="@php echo csrf_token(); @endphp">
-            <input type="text" placeholder="username" name="email"><br>
-            <input type="password" placeholder="password" name="password"><br>
-            <input type="submit" value="Login">
-        </form>
-
+        <div>Fed<span>Group</span></div>
+        <p class="description">Investments & Group Life</p>
     </div>
 @endsection
--- resources/views/layouts/auth/login.blade.php
+++ resources/views/layouts/auth/login.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Snapp Market | Log in</title>
+        <title>FedGroup</title>
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
         <link href="{{ asset('css/login.css') }}" rel="stylesheet">
     </head>
