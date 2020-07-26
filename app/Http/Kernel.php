2c0b5c13d23b3e50e6ae4e1b970135bace9390cc
Mohammad Alipour <md.alipour91@gmail.com>
2020-07-26 14:02:18 +0430

    fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,7 +2,6 @@
 
 namespace App\Http;
 
-use App\Http\Middleware\JwtMiddleware;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -20,7 +19,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
-        JwtMiddleware::class,
+        \App\Http\Middleware\JwtMiddleware::class,
     ];
 
     /**
