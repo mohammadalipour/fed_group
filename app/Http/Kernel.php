1dc574ff43b3042a837ac7435bf096ed88845c01
Mohammad Alipour <md.alipour91@gmail.com>
2020-07-26 14:06:51 +0430

    fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,7 +63,7 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'jwt.auth' => \Tymon\JWTAuth\Http\Middleware\Authenticate::class,
         'jwt.refresh' => \Tymon\JWTAuth\Http\Middleware\RefreshToken::class,
-        'jwt.verify' => \App\Http\Middleware\JwtMiddleware::class,
+        'JwtMiddleware' => \App\Http\Middleware\JwtMiddleware::class,
     ];
 
     /**
